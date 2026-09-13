f727e8fb85f4330638e4e8ae953bdbe9d0750d59
Mageofdark <darkflamesmage23@gmail.com>
2026-09-13 05:44:10 -0300

    refractor(clientes): Centralizacion de las peticiones de detalles y eliminacion

--- src/services/clientesService.js
+++ src/services/clientesService.js
@@ -15,7 +15,19 @@ const obtenerClientes = async () => {
   return respuesta.data;
 };
 
+const obtenerCliente = async (id) => {
+  const respuesta = await api.get(`/users/${id}`);
+  return respuesta.data;
+};
+
+const eliminarCliente = async (id) => {
+  const respuesta = await api.delete(`/users/${id}`);
+  return respuesta;
+};
+
 export default {
     crearCliente,
-    obtenerClientes
+    obtenerClientes,
+    obtenerCliente,
+    eliminarCliente
 };
